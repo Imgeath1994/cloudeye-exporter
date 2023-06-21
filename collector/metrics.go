2dfbdae0d0840def5cb9afc9d1d0e5f5656087e8
Aleksandr Sviridov <44110078+Imgeath1994@users.noreply.github.com>
2023-06-21 12:31:55 +0500

    Update metrics.go

--- collector/metrics.go
+++ collector/metrics.go
@@ -3,14 +3,14 @@ package collector
 import (
 	"sync"
 
-	"./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/core/auth/basic"
-	"./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/core/config"
-	ces "./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v1"
-	"./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v1/model"
-	cesv2 "./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v2"
-	cesv2model "./vendor/github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v2/model"
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/auth/basic"
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/core/config"
+	ces "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v1"
+	"github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v1/model"
+	cesv2 "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v2"
+	cesv2model "github.com/huaweicloud/huaweicloud-sdk-go-v3/services/ces/v2/model"
 
-	"./logs"
+	"github.com/Imgeath1994/cloudeye-exporter/logs"
 )
 
 var (
